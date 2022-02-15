01c18d55212ae85fd88b2583ab22b22a4db39ee6
Mokhtar Emad <mokhtaremad5@gmail.com>
2022-02-15 05:10:29 +0200

    Created IQRAA Webpage

---
+++ Admin.js
@@ -0,0 +1,9 @@
+function changecolor(id){
+document.body.style.background=document.getElementById(id).innerHTML;
+}
+function changetitlecolor(id){
+    document.getElementById('color1').style.background=document.getElementById(id).innerHTML;
+}
+function changemenucolor(id){
+    document.getElementById('color2').style.background=document.getElementById(id).innerHTML;
+}
